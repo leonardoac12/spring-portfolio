e269c378cf10f19d18b6d35d8f6dd6505a429ebc
leonardoac12 <leonardoarcr@outlook.es>
2025-04-14 16:27:43 -0500

    initial commit: Proyecto base - Portfolio

---
+++ src/test/java/com/springproy/Spring_Portfolio/SpringPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproy.Spring_Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
